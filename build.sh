4716dfd1f9672579f0c4d19a5bdfd0382f4cd091
Mike Seese <seesemichaelj@gmail.com>
2023-09-20 10:28:59 -0700

    add support for LinuxArm64

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # only used in windows for cross compiling
-export LINUX_MULTIARCH_ROOT=/c/UnrealToolchains/v21_clang-15.0.1-centos7/
+export LINUX_MULTIARCH_ROOT=/c/UnrealToolchains/v22_clang-16.0.6-centos7/
 if [ "$(uname)" != "Darwin" ]; then
   ${LINUX_MULTIARCH_ROOT}x86_64-unknown-linux-gnu/bin/clang++ -v
 fi
 
-export ENGINE_VERSION=5.2
+export ENGINE_VERSION=5.3
 export PLUGIN_NAME=$(basename $(pwd))
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -13,7 +13,7 @@ if [ "$(uname)" == "Darwin" ]; then
 else
   # windows handles linux cross compile
   UAT_PATH="/c/Program Files/Epic Games/UE_${ENGINE_VERSION}/Engine/Build/BatchFiles/RunUAT.bat"
-  TARGET_PLATFORMS=Win64+Linux
+  TARGET_PLATFORMS=Win64+Linux+LinuxArm64
 fi
 
 "${UAT_PATH}" \
